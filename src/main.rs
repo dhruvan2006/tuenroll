2f9a867754864e1528ca56b5733c9a03c6b302f8
Rodvdc <95091705+Rodvdc@users.noreply.github.com>
2024-11-10 19:10:17 +0100

    Added method to get cli credentials (#8)
    Co-authored-by: Rodrigue Vande Capelle <r.h.t.vandecapelle@student.tudelft.nl>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod api;
 mod models;
+use std::io;
 
 #[tokio::main]
 async fn main() {
-    let username = std::env::var("username").expect("Username not provided");
-    let password = std::env::var("password").expect("Password not provided");
+    // When using environment variables
+    //let username = std::env::var("username").expect("Username not provided");
+    //let password = std::env::var("password").expect("Password not provided");
+
+    // When using cli 
+    let (username, password) = get_credentials();
+  
     let access_token = api::get_access_token(username.as_str(), password.as_str()).await.expect("Fetching access token failed");
     println!("Access token: {}", access_token);
 
@@ -21,4 +27,22 @@ async fn main() {
     //     Ok(false) => println!("Test registrdation encountered issues."),
     //     Err(e) => println!("Failed to register for the test: {}", e),
     // }
+}
+
+
+
+fn get_credentials() -> (String, String) {
+    let mut username = String::new();
+    let mut password = String::new();
+
+    println!("Input username");
+    io::stdin().read_line(&mut username).expect("Couldn't read username");
+
+    println!("Input password");
+    io::stdin().read_line(&mut password).expect("Couldn't read password");
+
+    let username = username.trim_end().to_string();
+    let password = password.trim_end().to_string();
+
+    return (username, password);
 }
