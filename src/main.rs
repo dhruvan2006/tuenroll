0a6c56fd5ed25bed9a77e7ace53b4f5f10530f0e
Dhruvan Gnanadhandayuthapani <d.gnanadhandayuthapani@student.tudelft.nl>
2024-12-01 01:15:43 +0100

    Only import Stdio for windows

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,15 @@ use log::{error, info, warn};
 use notify_rust::Notification;
 use serde::{Deserialize, Serialize};
 use simplelog::*;
+use std::env;
 #[cfg(target_os = "linux")]
 use std::io;
 #[cfg(target_os = "linux")]
 use std::io::Write;
 #[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
-use std::{env, process::Stdio};
+#[cfg(target_os = "windows")]
+use std::process::Stdio;
 use std::{process::Command, thread, time};
 
 #[derive(Serialize, Deserialize)]
